0325f420686e4d90d3d11d0235685f5d09505027
clabroche <corentin.labroche@sailendra.fr>
2021-03-17 23:07:02 +0100

    fix(command): respond to isalive

--- src/discord.js
+++ src/discord.js
@@ -7,12 +7,34 @@ class MyDiscord {
     this.token = args.discordToken
     this.channelId = args.discordChannel
     this.isDiscordEnabled = args.discordToken && args.discordChannel
+    const self = this
+    this.commands = {
+      '/ISALIVE': {
+        cb: this.areYouAlive,
+        description: 'Check if i am alive'
+      },
+      '/HELP': {
+        cb: this.help,
+        description: 'Show this help'
+      },
+    }
   }
+  
   ready() {
     if(!this.isDiscordEnabled) return
     return new Promise((res) => {
       client.on('ready', async() => {
         console.log(`Logged in as ${client.user.tag}!`);
+        client.on('message', message => {
+          if (message.author.bot || message.channel.id !== args.discordChannel) return
+          const msg = message.content.toUpperCase().split(' ')[0]
+          console.log(msg)
+          Object.keys(this.commands).map(command => {
+            if (msg === command) {
+              this.commands[command].cb.call(this, message.channel, message.content.toLowerCase().replace(command.toLocaleLowerCase(), '').trim())
+            }
+          })
+        });
         const description = `
         ${require('../package.json').version}
         Launched with:
@@ -59,6 +81,26 @@ class MyDiscord {
       .setTimestamp()
       .setFooter('', icon);
   }
+  help() {
+    console.log(this.commands)
+    return this.send(new Discord.MessageEmbed()
+      .setColor('#faa329')
+      .setAuthor("Help", "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f6/Lol_question_mark.png/604px-Lol_question_mark.png")
+      .setDescription(Object.keys(this.commands).map(command => `- ${command.toLowerCase()}: ${this.commands[command].description}`).join('\n'))
+      .setTimestamp()
+    )
+  }
+  areYouAlive(channel, message) {
+    const possibilities = [
+      "Yes I'm alive ! And you ?",
+      "Maybe, I'm just a robot, I don't know...",
+      "Yes, but i tell you each days !",
+      "You boring me with your questions.",
+    ]
+    channel.send(possibilities[Math.floor(Math.random() * possibilities.length)]);
+  }
 } 
+
+
 module.exports = MyDiscord
 module.exports.discord = new MyDiscord()
